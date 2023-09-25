7d9c9cc4e062e15d0d46475718946a938c69a15d
jean carlos <jeancarlos3102@gmail.com>
2023-09-25 14:03:18 -0400

    working with get task form form

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -8,11 +8,18 @@ const TaskList = ({ tasks }: TaskListProps) => {
 	return (
 		<section>
 			<h2>Task List</h2>
+			<section>
+				<button>All</button>
+				<button>Done</button>
+				<button>Todo</button>
+			</section>
 			<ul>
 				{tasks.map((task, index) => (
 					<li key={index}>
 						<p>{task.task}</p>
+						<input type="checkbox" />
 						<button>Delete Task</button>
+						<button>Edit Task</button>
 					</li>
 				))}
 			</ul>
